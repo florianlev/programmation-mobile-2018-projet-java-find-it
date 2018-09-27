13ad9ca974930c1ef3100dfb5ecbf376cd8d8478
Antoine <aherkens@gmail.com>
2018-09-27 17:34:28 -0400

    nouveau projet : empty activity

---
+++ FindIt/app/src/main/java/ca/qc/cgmatane/informatique/findit/Vue_Accueil.java
@@ -0,0 +1,13 @@
+package ca.qc.cgmatane.informatique.findit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Vue_Accueil extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vue__accueil);
+    }
+}

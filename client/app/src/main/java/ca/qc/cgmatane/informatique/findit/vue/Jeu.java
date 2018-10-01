13cdb0c181ce4a1b51265872c744193db6e86073
cyrille <cyrille.hamel@protonmail.com>
2018-10-01 19:07:27 -0400

    ajout update possition joueur

--- client/app/src/main/java/ca/qc/cgmatane/informatique/findit/vue/Jeu.java
+++ client/app/src/main/java/ca/qc/cgmatane/informatique/findit/vue/Jeu.java
@@ -1,6 +1,7 @@
 package ca.qc.cgmatane.informatique.findit.vue;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.support.v4.app.ActivityCompat;
@@ -10,14 +11,22 @@ import android.support.v4.content.ContextCompat;
 import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.SettingsClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
 import ca.qc.cgmatane.informatique.findit.R;
 
@@ -25,6 +34,9 @@ public class Jeu extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private FusedLocationProviderClient mFusedLocationClient;
+    private LocationCallback mLocationCallback;
+    private Marker marqueurJoueur = null;
+    LocationRequest mLocationRequest = new LocationRequest();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +47,32 @@ public class Jeu extends FragmentActivity implements OnMapReadyCallback {
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        mLocationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                if (locationResult == null) {
+                    return;
+                }
+                for (Location location : locationResult.getLocations()) {
+                    double latitudeJoueur = location.getLatitude();
+                    double longitudeJoueur = location.getLongitude();
+                    LatLng possitionJoueur = new LatLng(latitudeJoueur, longitudeJoueur);
+                    Toast.makeText(Jeu.this, "latitude" + latitudeJoueur + " longitude" + longitudeJoueur, Toast.LENGTH_LONG).show();
+
+                    if (marqueurJoueur == null) {
+                        MarkerOptions options = new MarkerOptions().position(possitionJoueur).title("position joueur");
+                        marqueurJoueur = mMap.addMarker(options);
+                    } else {
+                        marqueurJoueur.setPosition(possitionJoueur);
+                    }
+                   // mMap.moveCamera(CameraUpdateFactory.newLatLng(possitionJoueur));
+                    // mMap.animateCamera(CameraUpdateFactory.zoomTo(16f));
+                }
+            }
+
+            ;
+        };
 
 
     }
@@ -53,35 +91,62 @@ public class Jeu extends FragmentActivity implements OnMapReadyCallback {
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         recupererPossitionJoueur();
+        createLocationRequest();
+        startLocationUpdates();
         /*// Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));*/
     }
 
-    public void recupererPossitionJoueur(){
+    public void recupererPossitionJoueur() {
 
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED) {
 
             ActivityCompat.requestPermissions(this, new String[]{
-                    Manifest.permission.ACCESS_FINE_LOCATION }, 1);
+                    Manifest.permission.ACCESS_FINE_LOCATION}, 1);
         }
 
         mFusedLocationClient.getLastLocation().addOnSuccessListener(
                 this, new OnSuccessListener<Location>() {
                     @Override
                     public void onSuccess(Location location) {
-
+                        if(location !=null){
                             double latitudeJoueur = location.getLatitude();
                             double longitudeJoueur = location.getLongitude();
-                            LatLng possitionJoueur=  new LatLng(latitudeJoueur,longitudeJoueur);
+                            LatLng possitionJoueur = new LatLng(latitudeJoueur, longitudeJoueur);
                             //Toast.makeText(Jeu.this, "latitude" + latitudeJoueur + " longitude" + longitudeJoueur, Toast.LENGTH_LONG).show();
-                            mMap.addMarker(new MarkerOptions().position(possitionJoueur).title("position joueur"));
+
+                            if (marqueurJoueur == null) {
+                                MarkerOptions options = new MarkerOptions().position(possitionJoueur).title("position joueur");
+                                marqueurJoueur = mMap.addMarker(options);
+                            } else {
+                                marqueurJoueur.setPosition(possitionJoueur);
+                            }
                             mMap.moveCamera(CameraUpdateFactory.newLatLng(possitionJoueur));
+                            mMap.animateCamera(CameraUpdateFactory.zoomTo(16f));
+
+                        }
 
 
                     }
                 });
     }
+
+    protected void createLocationRequest() {
+
+        mLocationRequest.setInterval(10000);
+        mLocationRequest.setFastestInterval(5000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder().addLocationRequest(mLocationRequest);
+    }
+
+    @SuppressLint("MissingPermission")
+    private void startLocationUpdates() {
+
+        mFusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallback, null);
+    }
+
+
 }

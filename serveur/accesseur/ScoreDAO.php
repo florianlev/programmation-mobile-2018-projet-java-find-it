0b401035cd0b547b617b5660b3b4c7a9a5f5a02f
Florian <flolevy33@gmail.com>
2018-10-10 20:47:53 -0400

    fix score ajouter php

--- serveur/accesseur/ScoreDAO.php
+++ serveur/accesseur/ScoreDAO.php
@@ -13,21 +13,14 @@ class ScoreDAO
 
     function ajouterScore($score)
     {
-        echo "ajouterScore()";
-        print_r($score);
 
-        $SQL_AJOUTER_SCORE = "INSERT into score(score, utilisateur_id) VALUES('$score->score','$score->utilisateur_id')";
+        $SQL_AJOUTER_SCORE = "INSERT into score(score, utilisateur_id) VALUES('$score->valeur','$score->utilisateur_id')";
 
-        echo $SQL_AJOUTER_SCORE;
         global $basededonnees;
-        print_r($basededonnees);
 
         $requeteAjouterScore = $basededonnees->prepare($SQL_AJOUTER_SCORE);
         $reussite = $requeteAjouterScore->execute();
 
-        echo "Code erreur : " . $basededonnees->errorCode();
-        echo "Erreurs : ";
-        print_r($basededonnees->errorInfo());
         return $reussite;
 
 

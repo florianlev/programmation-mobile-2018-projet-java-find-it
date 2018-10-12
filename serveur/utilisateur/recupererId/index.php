474301f9ee63c089e8316284dcac5facdb58eebd
Florian <flolevy33@gmail.com>
2018-10-11 23:16:31 -0400

    XML recupererID

---
+++ serveur/utilisateur/recupererId/index.php
@@ -0,0 +1,29 @@
+<?php
+
+include "../../accesseur/UtilisateurDAO.php";
+$utilisateurDAO = new UtilisateurDAO();
+
+$utilisateur = new stdClass();
+
+$utilisateur->pseudo = $_GET['pseudo'];
+$utilisateur->mail = $_GET['mail'];
+
+    
+$idUtilisateur = $utilisateurDAO->recupererIdUtilisateur($utilisateur);
+?>
+<?php 
+header("Content-type: text/xml");
+echo '<?xml version="1.0" encoding="UTF-8"?>';
+?>  
+<Utilisateurs>
+<?php
+    foreach ($idUtilisateur as $unIdUtilisateur) {
+?>
+    <utilisateur>
+        <id><?=$unIdUtilisateur->utilisateur_id?></id>
+    </utilisateur>
+<?php
+    }
+?>
+</Utilisateurs>
+
